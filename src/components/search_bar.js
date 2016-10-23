ef250541ef135c80409b6c724880f83df7e4485c
Prakash Koneru <PrakashKoneru@Sivas-MBP.home>
2016-10-23 14:12:53 -0400

    Implemented The css and Fetching data. Also implemented the search bar logic

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,17 +12,16 @@ class SearchBar extends Component {
 	}
 
 	render() {
-		return <div>
+		return <div className="search-bar">
 
-			 <input value = {this.state.term} onChange = {this.onInputChange.bind(this)} />
-			 Value of the term : {this.state.term}
+			 <input value = {this.state.term} onChange = {event => this.onInputChange(event.target.value)} />
+			 
 		</div>
 	}
 
-	onInputChange(event) {
-		this.setState({
-			term : event.target.value
-		});
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyD_5s_mmXSn1AumQlGEcqhxAwsoKHQEoZA'; 
 
@@ -12,19 +14,31 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo:null 
+		};
 
-		YTSearch({key : API_KEY, term : "surface"}, (videos) => {
-			this.setState({ videos });
-			console.log(videos);
+		this.videoSearch("Beat It Micheal Jackson");
+	}
+
+	videoSearch(term) {
+		YTSearch({key : API_KEY, term : term}, (videos) => {
+			this.setState({ 
+				videos,
+				selectedVideo:videos[0] 
+			});
 		});
+
+
 	}
 
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos = {this.state.videos}/>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos = {this.state.videos}/>
 			</div>
 		);
 	}
