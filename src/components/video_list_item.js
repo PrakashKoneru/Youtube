d32892e0df24b7f7c068477848888adef0f9daf4
Prakash Koneru <PrakashKoneru@Sivas-MBP.home>
2016-10-23 11:07:27 -0400

    First Commit. Implemented the redux boiler plate.

---
+++ src/components/video_list_item.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+const VideoListItem = ({video}) => {
+	// const video = props.video;
+	return <li>{props.video.etag} </li>
+	
+};
+
+export default VideoListItem;
